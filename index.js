e198d91f001ddb02125c3f7ea3c0695176c9cda8
Henri <henri.hamunen@gmail.com>
2022-01-13 21:46:55 +0200

    ex 3.8

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', function(req, res) {
+  if (req.method === 'POST') return JSON.stringify(req.body)
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 let persons = [
   { 
