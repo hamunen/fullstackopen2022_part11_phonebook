644e549efb512c95aa109d94b3007d14a125795f
Henri <henri.hamunen@gmail.com>
2022-01-13 20:54:55 +0200

    ex 3.2 - phonebook backend step 2

--- index.js
+++ index.js
@@ -24,8 +24,12 @@ let persons = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/info', (request, response) => {
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people </p>
+
+  <p>${Date()}</p>
+  `)
 })
 
 app.get('/api/persons', (request, response) => {
