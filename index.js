cf8ec05a3a8a9b4e3771fa498c68ca18ff39f208
Henri <henri.hamunen@gmail.com>
2022-01-13 20:57:29 +0200

    ex 3.3

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  person ? response.json(person) : response.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
