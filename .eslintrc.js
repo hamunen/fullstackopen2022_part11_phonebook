3e6e9ba529392dcc703a77ec89e4e936e7126b6e
Henri <henri.hamunen@gmail.com>
2023-04-01 17:08:43 +0300

    Add a dummy test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,23 @@
 module.exports = {
-  'env': {
-    'node': true,
-    'commonjs': true,
-    'es2021': true
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    'jest/globals': true,
   },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 13
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13,
   },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'eqeqeq': 'error',
+  rules: {
+    indent: ['error', 2],
+    eqeqeq: 'error',
     'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'linebreak-style': [
-      'error',
-      'windows'
-    ],
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'linebreak-style': ['error', 'windows'],
     'no-console': 0,
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ]
-  }
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Fullstackopen 2022 - part 11 - own pipeline
 
 Using phonebook app from part 2 & 3
+
+App url on fly.io: https://fullstackopen2022-phonebook-pipeline.fly.dev/
