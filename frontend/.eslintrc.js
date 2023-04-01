e70fb3e774747e1abc598cc25fee20b418416cb8
Henri <henri.hamunen@gmail.com>
2023-04-01 16:54:40 +0300

    Set up fly.io deployment

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,50 +1,32 @@
 /* eslint-disable no-undef */
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react/jsx-runtime',
   ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 13,
-    'sourceType': 'module'
+    ecmaVersion: 13,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react'
-  ],
-  'rules': {
+  plugins: ['react'],
+  rules: {
     'react/prop-types': 0,
-    'eqeqeq': 'error',
+    eqeqeq: 'error',
     'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': 0,
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'windows'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ]
-  }
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
 }
--- index.js
+++ index.js
@@ -11,38 +11,44 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-morgan.token('body', function(req) {
+morgan.token('body', function (req) {
   if (req.method === 'POST') return JSON.stringify(req.body)
 })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 app.get('/info', (request, response, next) => {
-  Person.count({}).then(result => {
-    response.send(response.send(`
+  Person.count({})
+    .then((result) => {
+      response.send(
+        response.send(`
       <p>Phonebook has info for ${result} people </p>
       <p>${Date()}</p>
-    `))
-  })
-    .catch(error => next(error))
+    `)
+      )
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
-  Person.find({}).then(people => {
-    response.json(people)
-  })
-    .catch(error => next(error))
+  Person.find({})
+    .then((people) => {
+      response.json(people)
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-    .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -50,9 +56,8 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .then(() => {
       response.status(204).end()
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
-
 
 app.post('/api/persons', (request, response, next) => {
   const person = new Person({
@@ -60,22 +65,24 @@ app.post('/api/persons', (request, response, next) => {
     number: request.body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-    .catch(error => next(error))
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndUpdate(
-    request.params.id
-    , { number: request.body.number }
-    , { runValidators: true, context: 'query', new: true }
+    request.params.id,
+    { number: request.body.number },
+    { runValidators: true, context: 'query', new: true }
   )
-    .then(updatedPerson => {
+    .then((updatedPerson) => {
       response.json(updatedPerson)
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -91,7 +98,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   }
   if (error.name === 'ValidationError') {
-    return response.status(400).send( { error: error.message })
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
@@ -100,7 +107,7 @@ const errorHandler = (error, request, response, next) => {
 // this has to be the last loaded middleware.
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
